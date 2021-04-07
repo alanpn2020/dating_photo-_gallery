94b9c3e47c785654ec1aa0610db252eebd10ab64
alanpn2020 <alanpn@outlook.com.br>
2021-04-07 11:30:37 -0300

    Dating photo album

---
+++ android/app/src/main/java/com/alanpn/album_photo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.alanpn.album_photo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red
+      ),
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: <Widget>[
+        Scaffold(
+            appBar: AppBar(
+              title: Text("Alan & Karoline"),
+              centerTitle: true,
+            ),
+            body: StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection('mensagens').snapshots(),
+                builder: (context, snapshot) {
+                  return !snapshot.hasData
+                      ? Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : StaggeredGridView.countBuilder(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 0,
+                          mainAxisSpacing: 12,
+                          itemCount: imageList.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(0))),
+                              child: ClipRRect(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(0)),
+                                child: FadeInImage.memoryNetwork(
+                                  placeholder: kTransparentImage,
+                                  image: snapshot.data.docs[index].get('image'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          },
+                          staggeredTileBuilder: (index) {
+                            return StaggeredTile.count(
+                                1, index.isEven ? 1.2 : 1.8);
+                          });
+                })),
+      ],
+    );
+  }
+}
+
+class FirebaseStorage {}
